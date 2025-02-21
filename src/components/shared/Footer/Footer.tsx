c27d3dea53150f5518bae7d7ffcadcd8467bac5c
edard3v <edard3v@gmail.com>
2025-02-21 15:25:41 -0500

    agg logout

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,5 +1,13 @@
+import { useAuthStore } from "@global_stores/auth/useAuthStore";
 import css from "./Footer.module.css";
+import Btn from "@components/buttons/Btn/Btn";
 
 export default function Footer() {
-  return <footer className={css.footer}>©️edard3v</footer>;
+  const logout = useAuthStore((state) => state.remove_token);
+  const token = useAuthStore((state) => state.token);
+  return (
+    <footer className={css.footer}>
+      {!token ? <div>©️edard3v</div> : <Btn onClick={logout}>Cerrar sesión</Btn>}
+    </footer>
+  );
 }
